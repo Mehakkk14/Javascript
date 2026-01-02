4b49e1ef6fb8c019debf9910912a5a611feb8d7c
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-01-02 12:24:36 +0530

    Initial commit

---
+++ The_Console.js
@@ -0,0 +1,26 @@
+/*The console.log() method outputs messages to the Console window.
+JavaScript code runs one line at a time, from top to bottom.
+Single line comments are created using the // forward slashes.
+Multi-line comments are started by and closed with .*/
+
+
+/*Use console.log() methods to print the following info:
+
+Name of the store/restaurant.
+A row that says "Order Details".
+A row with column names for "Item", "Quantity", and "Price".
+At least two items that were purchased.
+The total price for the purchase.*/
+
+console.log("==================================");
+console.log("           Burger King            ");
+console.log("----------------------------------");
+console.log("           Order Detauls          ");
+console.log("----------------------------------");
+console.log("Item           Quantity      Price");
+console.log("----------------------------------");
+console.log("Burger           1             250");
+console.log("Pizza puff       1             149");
+console.log("----------------------------------");
+console.log("Total                          399");
+console.log("==================================");
