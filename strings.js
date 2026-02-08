7a118a2eaa33a094666983c6616873829fa43f27
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-02-08 15:33:41 +0530

    Add strings.js

---
+++ strings.js
@@ -0,0 +1,55 @@
+/*let name = "Mahak";
+console.log(name)
+
+let friend = "Rohit";
+console.log(friend)
+
+// Template Literals or String Interpolation
+let girl1 = "Enjoy";
+let girl2 = "June";
+
+let sent = `${girl1} is a friend of ${girl2}`;
+console.log(sent)
+
+//escape sequence characters
+let school = 'St Ann\'s Day'
+console.log(school)
+
+let fruits = 'Apple\nBanana\nMango';
+console.log(fruits)
+
+//String methods
+
+let car = "Porsche 911";
+console.log(car.length)
+console.log(car.toUpperCase())
+console.log(car.toLowerCase())
+console.log(car.slice(2,4))
+console.log(car.replace("911", "913"))
+console.log(car.trim())
+
+let name1 = 'Suri';
+let name2 = 'Puri';
+console.log(name1.concat(name2, "Yes"))*/
+
+
+//practice 
+//1
+console.log("har\"".length)
+
+let str = 'Mahadev'
+console.log(str.includes("dev"))
+console.log(str.startsWith("Ma"))
+console.log(str.endsWith("dev"))
+
+let str2 = "Mahak is coding";
+console.log(str2.toLowerCase())
+
+let str3 = "Please give Rs 1000";
+console.log(Number.parseInt(str3.slice("Please give Rs".length)))
+console.log(typeof Number.parseInt(str3.slice("Please give Rs".length)))
+
+let str4 = 'Happy';
+console.log(str4.replace('y', 'ie'))
+
+
