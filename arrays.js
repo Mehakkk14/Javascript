f60bab99a4f6c3f05bcddb715289bd214a4184b6
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-02-09 20:38:25 +0530

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -17,3 +17,31 @@ console.log(nums.unshift(0))
 delete nums[2];
 console.log(nums);
 
+//loops in arrays
+
+let marks = [90, 80, 70, 60, 50];
+
+for (let i=0; i<marks.length; i++){
+    console.log(marks[i]);
+}
+
+
+marks.forEach((element)=>{
+    console.log(element*element);
+}
+)
+
+
+//array.from  --- used in dom to convert html collection into array
+
+let name = 'Mehak';
+let arr = Array.from(name);
+console.log(arr);
+
+for ( let i in arr){
+    console.log(arr[i]);
+}
+
+for (let a in arr){
+    console.log(a);
+}
