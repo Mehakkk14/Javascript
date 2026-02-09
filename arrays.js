e259f72ce4ba505322e649c46061b7be929a362e
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-02-09 20:17:49 +0530

    Add arrays.js

---
+++ arrays.js
@@ -0,0 +1,19 @@
+const fruits = ["apple", "mango", "kiwi"];
+console.log(fruits[0]);
+console.log(fruits.length);
+fruits[1] = "banana";
+console.log(fruits);
+console.log(typeof fruits)
+
+
+
+let nums = [1, 2, 3, 4, 5];
+console.log(nums.toString());
+console.log(nums.join("-"));
+console.log(nums.pop())
+console.log(nums.push(6))
+console.log(nums.shift())
+console.log(nums.unshift(0))
+delete nums[2];
+console.log(nums);
+
