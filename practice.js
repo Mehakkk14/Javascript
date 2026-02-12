69a5f5ba184e377ddf4d7a3232c27c67e13b8da0
Mahak Rastogi <rastogimehak3845@gmail.com>
2026-02-12 19:38:56 +0530

    Add practice.js, package files, and .gitignore

---
+++ practice.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')()
+
+function getRandom(min,max){
+  return Math.floor(Math.random()*max-min+1+min)
+}
+
+let RandomNum = getRandom(1,100);
+let guess;
+let attempts= 0;
+
+while(guess != RandomNum){
+  guess = parseInt(prompt("Guess a number between 1 and 100: "))
+  attempts++;
+  if(guess > RandomNum){
+    console.log("Too High!");
+  }
+  else if(guess < RandomNum){
+    console.log("Too Low!");
+  }
+  else{
+    console.log("Right guess");
+    let score = 100 - attempts;
+    console.log("Your score is: " + score);
+  }
+}
+
+
